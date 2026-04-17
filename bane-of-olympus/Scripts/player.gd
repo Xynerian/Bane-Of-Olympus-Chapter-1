5c747d748aec71f6be869f35062c66acff32e274
Ansh-droid-glitch <anshsingh131014@gmail.com>
2026-04-17 20:45:06 +0530

    added animation

--- bane-of-olympus/Scripts/player.gd
+++ bane-of-olympus/Scripts/player.gd
@@ -8,6 +8,8 @@ extends CharacterBody2D
 @export var jump_speed = -150.0
 @export var fall_speed = 100
 
+@onready var sprite = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor() and not Input.is_action_pressed("Jump") and not Input.is_action_pressed("Down"):
@@ -22,6 +24,12 @@ func _physics_process(delta: float) -> void:
 		velocity.y = jump_speed
 	var direction := Input.get_axis("Left", "Right")
 	if direction:
+		if(direction==1):
+			sprite.flip_h = false
+		else:
+			sprite.flip_h = true
+			
+		sprite.play("run")
 		velocity.x = move_toward(velocity.x, direction * speed, speed * acceleration)
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed * decceleration)
